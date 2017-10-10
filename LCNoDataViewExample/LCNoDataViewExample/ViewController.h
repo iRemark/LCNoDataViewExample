b3f2467d0159cd7f06a19f403ba8208e7dc18ff5
lc081200 <lc081200@163.com>
2017-10-10 01:35:16 -0500

    Add files via upload

---
+++ LCNoDataViewExample/LCNoDataViewExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCNoDataViewExample
+//
+//  Created by lc-macbook pro on 2017/10/10.
+//  Copyright © 2017年 http://www.cnblogs.com/saytome/. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+
+@end
+
